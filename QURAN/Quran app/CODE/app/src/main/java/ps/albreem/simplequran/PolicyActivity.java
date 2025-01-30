bfdc85a912fb9818931f1ff60efc1321b267c997
AHMEDALBREEM <amealbrem@gmail.com>
2025-01-30 17:21:30 +0600

    test

---
+++ QURAN/Quran app/CODE/app/src/main/java/ps/albreem/simplequran/PolicyActivity.java
@@ -0,0 +1,159 @@
+package ps.albreem.simplequran;
+
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.*;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+
+public class PolicyActivity extends Activity {
+	
+	private WebView webview1;
+	
+	private Intent vh = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.policy);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		webview1 = findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		webview1.loadUrl("file:///android_asset/a.html");
+		if (Build.VERSION.SDK_INT >= 21) { Window
+			w = this.getWindow();
+			w.setNavigationBarColor(Color.parseColor("#2D346C")); }
+		
+		
+		Animation animation;
+		        animation = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_in_left);
+		        animation.setDuration(900);
+		        webview1.startAnimation(animation);
+		        animation = null;
+		
+		
+		_effect(webview1, 40, 40, "#2D346C");
+	}
+	
+	@Override
+	public void onBackPressed() {
+		vh.setClass(getApplicationContext(), MainActivity.class);
+		vh.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(vh);
+		finish();
+	}
+	public void _effect(final View _view, final double _radius, final double _shadow, final String _color) {
+		android.graphics.drawable.GradientDrawable ab = new android.graphics.drawable.GradientDrawable();
+		
+		
+		
+		ab.setColor(Color.parseColor(_color));
+		
+		ab.setCornerRadius((float) _radius);
+		
+		_view.setElevation((float) _shadow);
+		
+		_view.setBackground(ab);
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
---
+++ QURAN/Quran app/README.md
@@ -0,0 +1,3 @@
+# Quran App 
+
+## Quran app the content are same you can just bulid it and change the style if you want 
